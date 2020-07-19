5933334be19bd34fa99a25bcebc87242d03381c2
Hemanth <hktechlabs@gmail.com>
2020-07-19 10:58:24 +0530

    index mobile view

--- js/index.js
+++ js/index.js
@@ -1,59 +1,75 @@
 $('.recent-items').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 5,
-    slidesToScroll: 5,
-    prevArrow: ".recent-list .slider-btn .pre",
-    nextArrow: ".recent-list .slider-btn .next",
-    dots:false,
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 6,
+  slidesToScroll: 6,
+  prevArrow: ".recent-list .slider-btn .pre",
+  nextArrow: ".recent-list .slider-btn .next",
+  dots:false,
 
-    responsive: [
-      {
-        breakpoint: 1124,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 4,
-        }
-      },
-      {
-        breakpoint: 900,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-        }
-      },
-      {
-        breakpoint: 650,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        }
-      },
-    //   {
-    //     breakpoint: 480,
-    //     settings: {
-    //       slidesToShow: 1,
-    //       slidesToScroll: 1
-    //     }
-    //   }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-  });
+  responsive: [
+    {
+      breakpoint: 1440,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 5,
+      }
+    },
+
+    {
+      breakpoint: 1124,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+      }
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      }
+    },
+    {
+      breakpoint: 650,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      }
+    },
+  //   {
+  //     breakpoint: 480,
+  //     settings: {
+  //       slidesToShow: 1,
+  //       slidesToScroll: 1
+  //     }
+  //   }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
      
   $('.recomended-items').slick({
     dots: true,
     infinite: true,
     speed: 300,
-    slidesToShow: 5,
-    slidesToScroll: 5,
+    slidesToShow: 6,
+    slidesToScroll: 6,
     prevArrow: ".recomended-list .slider-btn .pre",
     nextArrow: ".recomended-list .slider-btn .next",
     dots:false,
 
     responsive: [
+      {
+        breakpoint: 1440,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 5,
+        }
+      },
+
       {
         breakpoint: 1124,
         settings: {
@@ -92,13 +108,21 @@ $('.recent-items').slick({
     dots: true,
     infinite: true,
     speed: 300,
-    slidesToShow: 5,
-    slidesToScroll: 5,
+    slidesToShow: 6,
+    slidesToScroll: 6,
     prevArrow: ".deal-list .slider-btn .pre",
     nextArrow: ".deal-list .slider-btn .next",
     dots:false,
 
     responsive: [
+      {
+        breakpoint: 1440,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 5,
+        }
+      },
+
       {
         breakpoint: 1124,
         settings: {
